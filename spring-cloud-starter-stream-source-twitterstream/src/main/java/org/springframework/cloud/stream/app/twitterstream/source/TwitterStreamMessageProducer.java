422d976e079a4feb4d8847c195de3f8d0dd2b9b3
Nicolas Byl <nico@nicolas-byl.eu>
2017-11-16 16:59:36 +0100

    - replace spaces with tabs

--- spring-cloud-starter-stream-source-twitterstream/src/main/java/org/springframework/cloud/stream/app/twitterstream/source/TwitterStreamMessageProducer.java
+++ spring-cloud-starter-stream-source-twitterstream/src/main/java/org/springframework/cloud/stream/app/twitterstream/source/TwitterStreamMessageProducer.java
@@ -30,54 +30,54 @@ import java.net.URI;
  */
 class TwitterStreamMessageProducer extends AbstractTwitterInboundChannelAdapter {
 
-    private static final String API_URL_BASE = "https://stream.twitter.com/1.1/statuses/";
+	private static final String API_URL_BASE = "https://stream.twitter.com/1.1/statuses/";
 
-    private final TwitterStreamProperties twitterStreamProperties;
+	private final TwitterStreamProperties twitterStreamProperties;
 
-    TwitterStreamMessageProducer(TwitterTemplate twitterTemplate, TwitterStreamProperties twitterStreamProperties) {
-        super(twitterTemplate);
-        this.twitterStreamProperties = twitterStreamProperties;
-    }
+	TwitterStreamMessageProducer(TwitterTemplate twitterTemplate, TwitterStreamProperties twitterStreamProperties) {
+		super(twitterTemplate);
+		this.twitterStreamProperties = twitterStreamProperties;
+	}
 
-    @Override
-    protected URI buildUri() {
-        TwitterStreamType streamType = this.twitterStreamProperties.getStreamType();
-        URIBuilder b = URIBuilder.fromUri(API_URL_BASE + streamType.getPath());
-        //TODO: Support all the available properties
-        if (streamType == TwitterStreamType.FILTER) {
-            if (StringUtils.hasText(this.twitterStreamProperties.getFollow())) {
-                b.queryParam("follow", this.twitterStreamProperties.getFollow());
-            }
+	@Override
+	protected URI buildUri() {
+		TwitterStreamType streamType = this.twitterStreamProperties.getStreamType();
+		URIBuilder b = URIBuilder.fromUri(API_URL_BASE + streamType.getPath());
+		//TODO: Support all the available properties
+		if (streamType == TwitterStreamType.FILTER) {
+			if (StringUtils.hasText(this.twitterStreamProperties.getFollow())) {
+				b.queryParam("follow", this.twitterStreamProperties.getFollow());
+			}
 
-            if (StringUtils.hasText(this.twitterStreamProperties.getTrack())) {
-                b.queryParam("track", this.twitterStreamProperties.getTrack());
-            }
+			if (StringUtils.hasText(this.twitterStreamProperties.getTrack())) {
+				b.queryParam("track", this.twitterStreamProperties.getTrack());
+			}
 
-            if (StringUtils.hasText(this.twitterStreamProperties.getLocations())) {
-                b.queryParam("locations", this.twitterStreamProperties.getLocations());
-            }
-        } else {
-            if (StringUtils.hasText(this.twitterStreamProperties.getLanguage())) {
-                b.queryParam("language", this.twitterStreamProperties.getLanguage());
-            }
-        }
+			if (StringUtils.hasText(this.twitterStreamProperties.getLocations())) {
+				b.queryParam("locations", this.twitterStreamProperties.getLocations());
+			}
+		} else {
+			if (StringUtils.hasText(this.twitterStreamProperties.getLanguage())) {
+				b.queryParam("language", this.twitterStreamProperties.getLanguage());
+			}
+		}
 
-        URI uri = b.build();
-        logger.info("Using twitter stream url: " + uri);
-        return uri;
-    }
+		URI uri = b.build();
+		logger.info("Using twitter stream url: " + uri);
+		return uri;
+	}
 
-    @Override
-    protected void doSendLine(String line) {
-        if (line.startsWith("{\"limit")) {
-            logger.info("Twitter stream is being track limited.");
-        } else if (line.startsWith("{\"delete")) {
-            // discard
-        } else if (line.startsWith("{\"warning")) {
-            // discard
-        } else {
-            sendMessage(org.springframework.integration.support.MessageBuilder.withPayload(line).build());
-        }
-    }
+	@Override
+	protected void doSendLine(String line) {
+		if (line.startsWith("{\"limit")) {
+			logger.info("Twitter stream is being track limited.");
+		} else if (line.startsWith("{\"delete")) {
+			// discard
+		} else if (line.startsWith("{\"warning")) {
+			// discard
+		} else {
+			sendMessage(org.springframework.integration.support.MessageBuilder.withPayload(line).build());
+		}
+	}
 
 }
