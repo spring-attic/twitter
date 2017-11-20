c6ad8a380e3f251a58bc75187d2564ef17b0c929
Nicolas Byl <nico@nicolas-byl.eu>
2017-11-20 17:09:53 +0100

    Support language parameter for all stream types
    * remove obsolete tests as language is accepted for all endpoints
    
    * add language parameter for all stream types
    
    * organize import

--- spring-cloud-starter-stream-source-twitterstream/src/test/java/org/springframework/cloud/stream/app/twitterstream/source/TwitterStreamMessageProducerTests.java
+++ spring-cloud-starter-stream-source-twitterstream/src/test/java/org/springframework/cloud/stream/app/twitterstream/source/TwitterStreamMessageProducerTests.java
@@ -16,14 +16,13 @@
 
 package org.springframework.cloud.stream.app.twitterstream.source;
 
-import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import org.springframework.social.twitter.api.impl.TwitterTemplate;
 
 import java.net.URI;
 import java.net.URISyntaxException;
 
-import org.junit.Test;
-
-import org.springframework.social.twitter.api.impl.TwitterTemplate;
+import static org.junit.Assert.assertEquals;
 
 /**
  * @author Nicolas Byl
@@ -38,16 +37,6 @@ public class TwitterStreamMessageProducerTests {
 
 		TwitterStreamMessageProducer producer = new TwitterStreamMessageProducer(new TwitterTemplate(""), properties);
 		assertEquals(new URI("https://stream.twitter.com/1.1/statuses/sample.json?language=english"), producer.buildUri());
-	}
-
-	@Test
-	public void languageIsIgnoredOnFilter() throws URISyntaxException {
-		TwitterStreamProperties properties = new TwitterStreamProperties();
-		properties.setStreamType(TwitterStreamType.FILTER);
-		properties.setLanguage("english");
-
-		TwitterStreamMessageProducer producer = new TwitterStreamMessageProducer(new TwitterTemplate(""), properties);
-		assertEquals(new URI("https://stream.twitter.com/1.1/statuses/filter.json"), producer.buildUri());
 	}
 
 	@Test
